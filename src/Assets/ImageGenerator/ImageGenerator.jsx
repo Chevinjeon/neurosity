29437780e6f925588ff51901cb0f714b57b5c259
Chevin Jeon <cjeon1@ford.com>
2024-01-11 17:45:57 -0500

    hi

--- src/Assets/ImageGenerator/ImageGenerator.jsx
+++ src/Assets/ImageGenerator/ImageGenerator.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import './ImageGenerator.css'
+import default_image from './default_image.svg'
 
 const ImageGenerator = () => {
+
+    const [image_url, setImage_url] = useState("/")
+
+    const imageGenerator = async () =>{
+
+        const response = await fetch(
+            "https://api.openai.com/v1/images/generations",
+            {
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json",
+                    Authorization:
+                    "Bearer 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",
+                    "User-Agent":"Chrome",
+                },
+                body:JSON.stringify({
+                    prompt:`${inputRef.current.value}`,
+                    n:1,
+                    size:"512x512",
+                }), 
+            }
+        );
+        let data = await response.json();
+        console.log(data);
+ 
+
+    }
+
+
   return (
-    <div>ImageGenerator</div>
+    <div className='ai-image-generator'>
+        <div className="header">Emotion <span>Visualization</span></div>
+        <div className="img-loading">
+            <div className="image"><img src={image_url==="/"?default_image:image_url} alt="" /></div>
+        </div>
+        <div className="generate-btn" onClick={()=>{imageGenerator()}}>Generate</div>
+
+    </div>
   )
 }
 
